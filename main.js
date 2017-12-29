057bf307201fe6c72b2dce2b4c2acb5e0c93c0bb
L1224rd <edermarques96@gmail.com>
2017-12-29 17:10:57 -0200

    Initial Commit

--- main.js
+++ main.js
@@ -1,41 +1,33 @@
 function drawPixel(x, y, color, pixelSize) {
-    var c = document.getElementById("canvas");
+    var c = document.getElementById("canvas"); // canvas where the pixel will be set
     var ctx = c.getContext('2d');
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, pixelSize, pixelSize);
+    ctx.fillStyle = color; // pixel color
+    ctx.fillRect(x, y, pixelSize, pixelSize); // pixel position and size
 }
 
-function drawCode(text) {
+function drawCode(text) { // transforms the text into rgb numbers and pass to the getPixel function
     let pixelSize = 1;
-    let y = 0;
-    let n = 0;
-    for (let i = 0; i < text.length/3; i++) {
+    let y = 0; // n and y are used to make the bitmap like a rectangle
+    let n = 0; 
+    for (let i = 0; i < text.length / 3; i++) { // each rgb pixel gets 3 numbers
         if (n >= 999) {
-            y += pixelSize;
-            n = -1;
+            y += pixelSize; // when n gets to a certain point, y gets one line down
+            n = -1; // n is reset to start from the begining of the line
         }
-        
-        let rgb = 'rgb('
-        + getCharCode(text.substr(i * 3, 1)) + ','
-        + getCharCode(text.substr(i * 3 + 1, 1)) + ','
-        + getCharCode(text.substr(i * 3 + 2, 1)) + ')';
 
-        /* let rgb = 'rgb('
-        + text.substr(i * 9, 3)%255 + ','
-        + text.substr(i * 9 + 3, 3)%255 + ','
-        + text.substr(i * 9 + 6, 3)%255 + ')'; */
-
-        console.log(rgb);
+        let rgb = 'rgb(' // rgb transforms 3 letters into a pixel using their ascii codes
+            + getCharCode(text.substr(i * 3, 1)) + ','
+            + getCharCode(text.substr(i * 3 + 1, 1)) + ','
+            + getCharCode(text.substr(i * 3 + 2, 1)) + ')';
 
         drawPixel(n * pixelSize, y, rgb, pixelSize);
         n++;
     }
 }
 
-drawCode(genRandom(1800000));
-// drawCode('Eder Marques');
+drawCode(genRandom(1800000)); //passing a string with length of 1800000 for testing purposes
 
-function genRandom(n) {
+function genRandom(n) { // returns a random string with length of n
     let res = '';
     for (let i = 0; i < n; i++) {
         res += Math.floor((Math.random() * 10));
@@ -43,7 +35,7 @@ function genRandom(n) {
     return res;
 }
 
-function getCharCode(c) {
+function getCharCode(c) { // returns the ascii code of the char c
     return c.charCodeAt(0);
 }
 
